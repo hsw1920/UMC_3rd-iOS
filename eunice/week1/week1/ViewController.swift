9ff6e8824a715f7dabd3b0bb4359f15748448151
김민경 <kimminkyung@gimmingyeong-ui-MacBookAir.local>
2022-09-23 14:59:10 +0900

    upload week1

---
+++ eunice/week1/week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by 김민경 on 2022/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
