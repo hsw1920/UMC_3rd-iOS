e028c162e3ef554e5a9af597b419d06c6996cde1
hsw1920 <hsw1920@gmail.com>
2022-09-28 09:31:48 +0900

    feat: SideMenu 화면전환

--- seuhong/week2/week2-challenge/week2-challenge/MainViewController.swift
+++ seuhong/week2/week2-challenge/week2-challenge/MainViewController.swift
@@ -9,25 +9,32 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var sideMenuButton: UIBarButtonItem!
 
+    
     @IBOutlet weak var registerBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        customSetting()
+        
+            }
+    
+    
+    @IBAction func sideMenuBtnTapped(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SideMenuViewController")
+        
+        //present(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func customSetting() {
         registerBtn.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 30, bottom: 10, trailing: 30)
         registerBtn.configuration?.background.strokeColor = .white
         registerBtn.configuration?.background.strokeWidth = 1
-            }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
 }
+
