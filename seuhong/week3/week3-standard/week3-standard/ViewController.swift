301838b9d35aec9670ce8e3d395d25da4a6dc938
hsw1920 <hsw1920@gmail.com>
2022-10-04 12:43:25 +0900

    initial commit: week3-standard

---
+++ seuhong/week3/week3-standard/week3-standard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3-standard
+//
+//  Created by 홍승완 on 2022/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
