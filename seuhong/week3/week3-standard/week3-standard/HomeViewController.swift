3192630fc5e442e9b50fd7dc1e9b36db38365b5a
hsw1920 <hsw1920@gmail.com>
2022-10-04 16:56:58 +0900

    add) week3-standard

--- seuhong/week3/week3-standard/week3-standard/HomeViewController.swift
+++ seuhong/week3/week3-standard/week3-standard/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  week3-standard
 //
 //  Created by 홍승완 on 2022/10/04.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
