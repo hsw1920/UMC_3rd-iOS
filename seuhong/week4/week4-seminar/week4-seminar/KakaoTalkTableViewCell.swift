f59adb275daffa8b17245ab0a3821a1ff71b4aba
hsw1920 <hsw1920@gmail.com>
2022-10-11 18:25:01 +0900

    week4-standard init

---
+++ seuhong/week4/week4-seminar/week4-seminar/KakaoTalkTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  KakaoTalkTableViewCell.swift
+//  week4-seminar
+//
+//  Created by 홍승완 on 2022/10/11.
+//
+
+import UIKit
+
+class KakaoTalkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
