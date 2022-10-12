d9a657c5a7525ac7354a69e401b8ff2916628d0f
hsw1920 <hsw1920@gmail.com>
2022-10-12 20:42:30 +0900

    week4-mission-delete

--- seuhong/week3/week3-challenge/week3-challenge/DetailMemo/DetailMemoViewController.swift
+++ seuhong/week3/week3-challenge/week3-challenge/DetailMemo/DetailMemoViewController.swift
@@ -13,7 +13,17 @@ class DetailMemoViewController: UIViewController {
     
     var homeMemo = Memo.list
     var detailMemoList = DetailMemo.MemoList
-    var index = Int()
+    var index = IndexPath()
+    
+    var delegate: DeleteMemoProtocol?
+    
+    enum CurrentState {
+        case edit
+        case delete
+    }
+    
+    var state = CurrentState.edit
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,20 +31,42 @@ class DetailMemoViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         print("DetailVC will appear")
-        detailTitle.text = detailMemoList[index].detailTitle
-        detailMemo.text = detailMemoList[index].detailMemo
+        for i in homeMemo {
+            print(i.titleLabel)
+        }
+
+        detailTitle.text = detailMemoList[index.row].detailTitle
+        detailMemo.text = detailMemoList[index.row].detailMemo
         print("Previous contents were loaded")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         print("DetailVC will disappear")
-        //home title 변경
-        homeMemo[index].titleLabel = detailTitle.text!
-        //detail 변경
-        detailMemoList[index].detailTitle = detailTitle.text!
-        detailMemoList[index].detailMemo = detailMemo.text!
+        
+        switch state {
+        case .edit :
+            print("현재는 편집상태임")
+            //home title 변경
+            homeMemo[index.row].titleLabel = detailTitle.text!
+            //detail 변경
+            detailMemoList[index.row].detailTitle = detailTitle.text!
+            detailMemoList[index.row].detailMemo = detailMemo.text!
+            
+        case .delete:
+            print("현재는 삭제된 상태")
+            delegate?.deleteMemo(indexPath: index)
+            
+        }
         print("Contents were modified")
         print("-----------------")
     }
     
+    @IBAction func deleteBtnTapped(_ sender: UIBarButtonItem) {
+        detailMemoList.remove(at: index.row)
+        homeMemo.remove(at: index.row)
+        print(detailMemoList.count)
+        print(homeMemo.count)
+        state = CurrentState.delete
+        self.navigationController?.popViewController(animated: true)
+    }
 }
--- seuhong/week3/week3-challenge/week3-challenge/MemoHome/MemoViewController.swift
+++ seuhong/week3/week3-challenge/week3-challenge/MemoHome/MemoViewController.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
-class MemoViewController: UIViewController, NewMemoProtocol {
+protocol DeleteMemoProtocol {
+    func deleteMemo(indexPath: IndexPath)
+}
+
+class MemoViewController: UIViewController, NewMemoProtocol, DeleteMemoProtocol {
+    
+    func deleteMemo(indexPath: IndexPath) {
+        list.remove(at: indexPath.row)
+        detailList.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -18,8 +28,6 @@ class MemoViewController: UIViewController, NewMemoProtocol {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
-        //tableView.reloadData()
     }
     
     @IBAction func addMemo(_ sender: UIBarButtonItem) {
@@ -33,6 +41,7 @@ class MemoViewController: UIViewController, NewMemoProtocol {
     override func viewWillAppear(_ animated: Bool) {
         print("MemoHome will appear")
         tableView.reloadData()
+        
         print("Table contents are reloaded")
         print("-----------------")
     }
@@ -54,22 +63,44 @@ extension MemoViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "DetailMemoViewController") as? DetailMemoViewController else { return }
-        
-        vc.index = indexPath.row
+        vc.delegate = self
+        vc.index = indexPath
         vc.homeMemo = list
         vc.detailMemoList = detailList
         navigationController?.pushViewController(vc, animated: true)
     }
+ 
 }
 extension MemoViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
     }
     
+    // Swipe 삭제
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            list.remove(at: indexPath.row)
+            detailList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            
+        }
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath) as? MemoCell else {
             return UITableViewCell()
         }
+        
+        // 재사용셀 문제 해결 -> 특정 조건에서 변화를 주었다면,
+        // 그 조건에 해당하지 않을 때는 초기화해주는 코드를 작성해야함.
+        // 또는 prepareForReuse() 메서드를 활용할 수 있음
+        if indexPath.row == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .systemBackground
+        }
+        
         cell.configure(item: list[indexPath.row])
         return cell
     }
